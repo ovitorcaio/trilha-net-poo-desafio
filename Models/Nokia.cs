77322b7beaecfd92a983f271c7e3e9a77e8e91c7
Caio Vítor Costa Soares <caio.vitor@live.com>
2025-09-21 22:45:15 -0300

    Entrega do código.

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Herança implementada adicionando ": Smartphone"
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // ATENÇÃO: É obrigatório adicionar um construtor para passar os dados para a classe-mãe "Smartphone"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Método "InstalarAplicativo" sobrescrito
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Nokia.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,39 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class teste
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Teste com Smartphone Nokia ---");
+        // Cria uma instância da classe Nokia, passando os parâmetros necessários
+        Smartphone nokia = new Nokia(numero: "99999-1234", modelo: "Nokia 666", imei: "111111111111111", memoria: 64);
+        
+        // Exibe o número do celular
+        Console.WriteLine($"Número: {nokia.Numero}");
+
+        // Chama os métodos herdados da classe Smartphone
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+
+        // Chama o método sobrescrito específico da classe Nokia
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n-------------------------------------\n");
+
+        Console.WriteLine("--- Teste com Smartphone iPhone ---");
+        // Cria uma instância da classe Iphone, passando os parâmetros necessários
+        Smartphone iphone = new Iphone(numero: "99999-5678", modelo: "iPhone 17 Pro", imei: "222222222222222", memoria: 256);
+        
+        // Exibe o número do celular
+        Console.WriteLine($"Número: {iphone.Numero}");
+
+        // Chama os métodos herdados da classe Smartphone
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+
+        // Chama o método sobrescrito específico da classe Iphone
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
